142e60e84b40dcd7f91d892ebfee9645a11d9d7e
Somnir <matthieu.burtin.somnir@gmail.com>
2017-01-06 15:12:13 +0100

    Résolution CSS

--- tripbook/resources/views/home.php
+++ tripbook/resources/views/home.php
@@ -3,8 +3,28 @@
 <head>
 <meta charset="UTF-8" />
 		<meta name="description" content="165c. uniques">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
         <script type="text/javascript" src="jquery-3.1.1.js"></script>
+         <script type="text/javascript">
+            $(document).ready(function(){  
+            $("a").click(function(){   
+            page=($(this).attr("href")); 
+            $.ajax({ 
+            url: page, 
+            cache: false, 
+            success:function(html){
+            afficher(html);     
+            },
+            error:function(XMLHttpRequest, textStatus, errorThrows){ 
+            }
+            });
+            return false; 
+            });
+            });function afficher(donnees){ 
+            $("#contenu").empty(); 
+            $("#contenu").append(donnees); 
+            }
+            </script>
 		<title>TripBook</title>
 </head>
 <body>
